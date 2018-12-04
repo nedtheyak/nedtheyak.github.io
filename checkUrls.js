118812c5463827321cdafe05e21fde277f940797
Ned the Yak <nedtheyak@magmablock.x10host.com>
2018-12-04 09:11:02 -0800

    Removed kydn.me from all urls, localized it to the "slight" url

--- checkUrls.js
+++ checkUrls.js
@@ -4,13 +4,12 @@ var loadPage = true;
 function redirectUsing(given) {
     switch (given) {
         case "s":
-            newUrl = "slight"
+            newUrl = "http://kydn.me/slight"
             break;
         default:
             // do nothing
             break;
     }
-    newUrl = `http://kydn.me/${newUrl}`;
     if (newUrl != "") {
         loadPage = false;
     }
