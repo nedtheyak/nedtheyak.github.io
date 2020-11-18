f299851cc7c40ac1f47de6bf3a1f69d3a6c29988
Nedtheyak <kschmidt36@my.bcit.ca>
2020-11-18 12:35:37 -0800

    Change urls to match pathname

--- checkUrls.js
+++ checkUrls.js
@@ -3,28 +3,28 @@ var loadPage = true;
 
 function redirectUsing(given) {
     switch (given) {
-        case "s":
-        case "slight":
-            newUrl = "https://nedtheyak.itch.io/slight/";
-            break;
-        case "pm":
-            newUrl = "http://prsa.me";
-            break;
-        case "m":
-            newUrl = "http://magmablock.x10host.com";
-            break;
-        case "h":
-        case "home":
+        case "/h":
+        case "/home":
             newUrl = "https://kaydens.ca"
             break;
-        case "calc":
+        case "/s":
+        case "/slight":
+            newUrl = "https://nedtheyak.itch.io/slight/";
+            break;
+        case "/pm":
+            newUrl = "http://prsa.me";
+            break;
+        case "/m":
+            newUrl = "http://magmablock.x10host.com";
+            break;
+        case "/calc":
             newUrl = "https://kaydenlukeschmidt.wixsite.com/website"
             break;
-        case "bcit/comp1536/Zombie":
-        case "bcit/comp1536/zombie":
+        case "/bcit/comp1536/Zombie":
+        case "/bcit/comp1536/zombie":
             newUrl = "https://kaydens.ca/Zombie"
             break;
-        case "bcit/comp1536/assignments/lab8":
+        case "/bcit/comp1536/assignments/lab8":
             newUrl = "https://kaydens.ca/assignments/lab8";
             break;
         default:
