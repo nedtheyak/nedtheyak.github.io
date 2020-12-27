6921616c4a0882c9dc433ddca5e27070d05a19fa
Nedtheyak <kschmidt36@my.bcit.ca>
2020-12-27 09:36:05 -0800

    Add new Dressember direct links

--- checkUrls.js
+++ checkUrls.js
@@ -13,6 +13,28 @@ function redirectUsing(given) {
 		case "/december":
 			newUrl = "https://kaydens.ca/dressember";
 			break;
+		case "/dress/amy":
+		case "/dressember/amy":
+			newUrl = "https://dressemberijm-2020.funraise.org/fundraiser/amy-forsyth/";
+			break;
+		case "/dress/ellie":
+		case "/dressember/ellie":
+		case "/dress/aurelia":
+		case "/dressember/aurelia":
+			newUrl = "https://dressemberijm-2020.funraise.org/fundraiser/aurelia-forsyth/";
+			break;
+		case "/dress/jason":
+		case "/dressember/jason":
+			newUrl = "https://dressemberijm-2020.funraise.org/fundraiser/jason-schmidt/";
+			break;
+		case "/dress/kayden":
+		case "/dressember/kayden":
+			newUrl = "https://dressemberijm-2020.funraise.org/fundraiser/kayden-schmidt/";
+			break;
+		case "/dress/team":
+		case "/dressember/team":
+			newUrl = "https://dressemberijm-2020.funraise.org/team/man-its-cold/";
+			break;
         case "/s":
         case "/slight":
             newUrl = "https://nedtheyak.itch.io/slight/";
